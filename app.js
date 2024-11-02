3a20ed31ca36f07367256f80d2bebbde985d69d6
ishanrana363 <ishanrana363@gmail.com>
2024-11-02 15:43:05 +0600

    project initial and file folder created

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express")
+const rateLimit = require("express-rate-limit")
+const xss = require('xss-clean')
+const helmet = require("helmet")
+const hpp = require('hpp');
+const cors = require("cors")
+const mongoSanitize = require('express-mongo-sanitize');
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+
+
+const app = new express();
+
+
+// Using rate limit middleware
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+})
+
+app.use(limiter)
+
+// Using helmet for secure http response
+
+
+// Using xss-clean sanitize for body query params
+
+app.use(xss())
+
+// Using hpp for protect against HTTP Parameter Pollution attacks query req.body params
+
+app.use(hpp())
+
+// Using cors for enabling CORS
+
+app.use(cors())
+
+// Using MongoSanitize for sanitize user input
+
+app.use(mongoSanitize())
+
+
+
+
+app.use(express.urlencoded({ limit: '1000mb' }));
+app.use(express.json({ limit: '1000mb' }));
+// Database Connect
+const db = process.env.DATABASE;
+
+mongoose.connect(db).then((res)=>{
+    console.log("---Connected to database----");
+}).catch((err)=>{
+    console.log("Error connecting to database", err);
+});
+
+
+
+// api file import
+
+const routes = require("./src/routes/api");
+
+app.use("/api/v1", routes)
+
+
+
+
+
+module.exports = app
